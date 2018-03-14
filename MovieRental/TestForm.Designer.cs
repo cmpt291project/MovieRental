ad4e098f0f13ca0d3b22f953d955e6be7d893177
myles alexander <mylesh86@yahoo.com>
2018-03-13 21:30:29 -0600

    testing different panel sizes

--- MovieRental/TestForm.Designer.cs
+++ MovieRental/TestForm.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace MovieRental
 {
-    partial class FeatureControl
+    partial class TestForm
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,10 +20,10 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
+        #region Windows Form Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
@@ -33,23 +33,20 @@
             // 
             // MoviePanel
             // 
-            this.MoviePanel.AutoScroll = true;
-            this.MoviePanel.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.MoviePanel.Location = new System.Drawing.Point(95, 0);
+            this.MoviePanel.Location = new System.Drawing.Point(3, 3);
             this.MoviePanel.Name = "MoviePanel";
-            this.MoviePanel.Size = new System.Drawing.Size(611, 391);
+            this.MoviePanel.Size = new System.Drawing.Size(577, 404);
             this.MoviePanel.TabIndex = 0;
-            this.MoviePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MoviePanel_Paint);
             // 
-            // FeatureControl
+            // TestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(584, 409);
             this.Controls.Add(this.MoviePanel);
-            this.Name = "FeatureControl";
-            this.Size = new System.Drawing.Size(722, 448);
-            this.Load += new System.EventHandler(this.FeatureControl_Load);
+            this.Name = "TestForm";
+            this.Text = "TestForm";
+            this.Load += new System.EventHandler(this.TestForm_Load);
             this.ResumeLayout(false);
 
         }
---
+++ MovieRental/TestForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace MovieRental
+{
+    public partial class TestForm : Form
+    {
+        public static string connectionString;
+
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            connectionString = ConfigurationManager.
+                ConnectionStrings["MovieRental.Properties." +
+                "Settings.MovieRentalConnectionString"].ConnectionString;
+            Console.WriteLine(connectionString);
+            if (!MoviePanel.Controls.Contains(FeatureControl.Instance))
+            {
+                MoviePanel.Controls.Add(FeatureControl.Instance);
+                FeatureControl.Instance.Dock = DockStyle.Fill;
+                FeatureControl.Instance.BringToFront();
+            }
+            else
+                FeatureControl.Instance.BringToFront();
+
+            
+        }
+    }
+}
