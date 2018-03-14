2a0ab2a9e046704e67efeda759cf7a89636cb524
myles alexander <mylesh86@yahoo.com>
2018-03-13 19:03:25 -0600

    uc2

--- MovieRental/UC2.Designer.cs
+++ MovieRental/UC2.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.Form2Tab1 = new System.Windows.Forms.TabControl();
             this.Features = new System.Windows.Forms.TabPage();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button6 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.Suggestion = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,12 +39,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.YourMoviePanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.YourMoviePanel = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
             this.Form2Tab1.SuspendLayout();
             this.Features.SuspendLayout();
             this.Suggestion.SuspendLayout();
-            this.YourMovie.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -69,50 +68,12 @@
             this.Features.Controls.Add(this.button6);
             this.Features.Controls.Add(this.panel2);
             this.Features.Location = new System.Drawing.Point(4, 22);
-
             this.Features.Name = "Features";
-            this.Features.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Features.Padding = new System.Windows.Forms.Padding(3);
             this.Features.Size = new System.Drawing.Size(731, 458);
-
             this.Features.TabIndex = 0;
             this.Features.Text = "Feature";
             this.Features.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Action",
-            "Adventure",
-            "Comedy",
-            "Drama",
-            "Horror",
-            "Thriller",
-            "Action",
-            "Adventure",
-            "Comedy",
-            "Drama",
-            "Horror",
-            "Thriller"});
-
-            this.comboBox1.Location = new System.Drawing.Point(3, 39);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(64, 21);
-
-            this.comboBox1.TabIndex = 2;
-            // 
-            // button6
-            // 
-
-            this.button6.Location = new System.Drawing.Point(3, 10);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(63, 23);
-
-            this.button6.TabIndex = 1;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // panel2
             // 
@@ -120,7 +81,6 @@
             this.panel2.Location = new System.Drawing.Point(72, 4);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(653, 448);
-
             this.panel2.TabIndex = 0;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
@@ -130,9 +90,8 @@
             this.Suggestion.Controls.Add(this.rank);
             this.Suggestion.Location = new System.Drawing.Point(4, 22);
             this.Suggestion.Name = "Suggestion";
-            this.Suggestion.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Suggestion.Padding = new System.Windows.Forms.Padding(3);
             this.Suggestion.Size = new System.Drawing.Size(731, 458);
-
             this.Suggestion.TabIndex = 1;
             this.Suggestion.Text = "Suggestion";
             this.Suggestion.UseVisualStyleBackColor = true;
@@ -151,7 +110,7 @@
             // rank
             // 
             this.rank.Location = new System.Drawing.Point(11, 43);
-            this.rank.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rank.Margin = new System.Windows.Forms.Padding(2);
             this.rank.Name = "rank";
             this.rank.Size = new System.Drawing.Size(698, 210);
             this.rank.TabIndex = 0;
@@ -161,25 +120,19 @@
             this.YourMovie.AutoScroll = true;
             this.YourMovie.Location = new System.Drawing.Point(4, 22);
             this.YourMovie.Name = "YourMovie";
-            this.YourMovie.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.YourMovie.Padding = new System.Windows.Forms.Padding(3);
             this.YourMovie.Size = new System.Drawing.Size(731, 458);
             this.YourMovie.TabIndex = 2;
             this.YourMovie.Text = "Your Movie";
             this.YourMovie.UseVisualStyleBackColor = true;
             this.YourMovie.Click += new System.EventHandler(this.tabPage3_Click);
             // 
-
-            // button5
-            // 
-           
-
             // panel1
             // 
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.pictureBox1);
-
             this.panel1.Location = new System.Drawing.Point(391, 33);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 23);
@@ -187,7 +140,6 @@
             // 
             // button2
             // 
-
             this.button2.Location = new System.Drawing.Point(151, 1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(33, 21);
@@ -206,19 +158,10 @@
             // 
             // textBox1
             // 
-
             this.textBox1.Location = new System.Drawing.Point(0, 3);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(70, 20);
             this.textBox1.TabIndex = 1;
-            // 
-            // YourMoviePanel
-            // 
-            this.YourMoviePanel.Location = new System.Drawing.Point(9, 10);
-            this.YourMoviePanel.Name = "YourMoviePanel";
-            this.YourMoviePanel.Size = new System.Drawing.Size(862, 483);
-            this.YourMoviePanel.TabIndex = 3;
-            this.YourMoviePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
             // pictureBox1
             // 
@@ -229,6 +172,46 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // YourMoviePanel
+            // 
+            this.YourMoviePanel.Location = new System.Drawing.Point(9, 10);
+            this.YourMoviePanel.Name = "YourMoviePanel";
+            this.YourMoviePanel.Size = new System.Drawing.Size(862, 483);
+            this.YourMoviePanel.TabIndex = 3;
+            this.YourMoviePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Action",
+            "Adventure",
+            "Comedy",
+            "Drama",
+            "Horror",
+            "Thriller",
+            "Action",
+            "Adventure",
+            "Comedy",
+            "Drama",
+            "Horror",
+            "Thriller"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(64, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // UC2
             // 
@@ -243,7 +226,6 @@
             this.Features.ResumeLayout(false);
             this.Suggestion.ResumeLayout(false);
             this.Suggestion.PerformLayout();
-            this.YourMovie.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -263,10 +245,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Panel rank;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel YourMoviePanel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button6;
     }
 }
