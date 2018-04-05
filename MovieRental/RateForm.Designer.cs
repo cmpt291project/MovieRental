e3b794840cb2831dd57d146da319558203a339f7
yan zhang <zhangy74@mymacewan.ca>
2018-04-04 19:53:38 -0600

    rate function

--- MovieRental/RateForm.Designer.cs
+++ MovieRental/RateForm.Designer.cs
@@ -32,8 +32,12 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.submit = new System.Windows.Forms.Button();
             this.rateMovie = new System.Windows.Forms.GroupBox();
-            this.movieName = new System.Windows.Forms.Label();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.movieName = new System.Windows.Forms.Label();
             this.rateMovie.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,16 +47,16 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.Location = new System.Drawing.Point(28, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(402, 31);
+            this.label1.Size = new System.Drawing.Size(826, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Please rate the movie and stars.";
+            this.label1.Text = "Please rate the movie and stars. To skip this just close the window. ";
             // 
             // panel1
             // 
             this.panel1.AutoSize = true;
-            this.panel1.Location = new System.Drawing.Point(22, 246);
+            this.panel1.Location = new System.Drawing.Point(12, 205);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(766, 355);
+            this.panel1.Size = new System.Drawing.Size(776, 355);
             this.panel1.TabIndex = 1;
             // 
             // submit
@@ -63,9 +67,14 @@
             this.submit.TabIndex = 0;
             this.submit.Text = "Submit";
             this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
             // 
             // rateMovie
             // 
+            this.rateMovie.Controls.Add(this.radioButton5);
+            this.rateMovie.Controls.Add(this.radioButton4);
+            this.rateMovie.Controls.Add(this.radioButton3);
+            this.rateMovie.Controls.Add(this.radioButton2);
             this.rateMovie.Controls.Add(this.radioButton1);
             this.rateMovie.Controls.Add(this.movieName);
             this.rateMovie.Location = new System.Drawing.Point(22, 72);
@@ -75,19 +84,54 @@
             this.rateMovie.TabStop = false;
             this.rateMovie.Text = "Rate The Movie";
             // 
-            // movieName
+            // radioButton5
             // 
-            this.movieName.AutoSize = true;
-            this.movieName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.movieName.Location = new System.Drawing.Point(6, 39);
-            this.movieName.Name = "movieName";
-            this.movieName.Size = new System.Drawing.Size(81, 31);
-            this.movieName.TabIndex = 0;
-            this.movieName.Text = "name";
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton5.Location = new System.Drawing.Point(422, 82);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(57, 33);
+            this.radioButton5.TabIndex = 3;
+            this.radioButton5.Text = "1";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton4.Location = new System.Drawing.Point(307, 82);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(57, 33);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.Text = "2";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton3.Location = new System.Drawing.Point(206, 82);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 33);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton2.Location = new System.Drawing.Point(106, 82);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 33);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "4";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.radioButton1.Location = new System.Drawing.Point(12, 82);
             this.radioButton1.Name = "radioButton1";
@@ -96,6 +140,16 @@
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "5";
             this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // movieName
+            // 
+            this.movieName.AutoSize = true;
+            this.movieName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.movieName.Location = new System.Drawing.Point(6, 39);
+            this.movieName.Name = "movieName";
+            this.movieName.Size = new System.Drawing.Size(85, 31);
+            this.movieName.TabIndex = 0;
+            this.movieName.Text = "name";
             // 
             // RateForm
             // 
@@ -125,5 +179,9 @@
         private System.Windows.Forms.GroupBox rateMovie;
         private System.Windows.Forms.Label movieName;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
