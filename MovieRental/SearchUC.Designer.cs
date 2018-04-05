c6db081a14fac7572c83e0c90f349770f7042526
myles alexander <mylesh86@yahoo.com>
2018-04-05 08:59:00 -0600

    merge conflict

--- MovieRental/SearchUC.Designer.cs
+++ MovieRental/SearchUC.Designer.cs
@@ -57,7 +57,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "SearchUC";
-            this.Size = new System.Drawing.Size(635, 552);
+            this.Size = new System.Drawing.Size(700, 564);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
